e13a3ec33152ce205ea815ce7da947de1ccb656e
johnou <johno@hellface.com>
2013-10-01 16:03:05 +0200

    Initial commit

---
+++ src/main/java/com/sulake/jenkins/plugins/jeffplugin/SmugDecorator.java
@@ -0,0 +1,11 @@
+package com.sulake.jenkins.plugins.jeffplugin;
+
+import hudson.Extension;
+import hudson.model.PageDecorator;
+
+/**
+ * @author Johno Crawford (johno@sulake.com)
+ */
+@Extension
+public class SmugDecorator extends PageDecorator {
+}
